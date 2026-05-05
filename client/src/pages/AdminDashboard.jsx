b64b03275bca09b0621ee6d5c61056452c76c5c6
s3mivra <s3mivra@gmail.com>
2026-05-06 07:00:42 +0800

    0.40 patch

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -5,8 +5,8 @@ import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 //const API_URL ='http://192.168.254.116:5002';
 //const API_URL = 'http://192.168.68.127:5002';
-//const API_URL = 'https://local-based-menu.onrender.com';
-const API_URL = 'http://192.168.100.2:5002'; // Change back to Render URL when deploying!
+const API_URL = 'https://local-based-menu.onrender.com';
+//const API_URL = 'http://192.168.100.2:5002'; // Change back to Render URL when deploying!
 //const API_URL = 'http://10.201.1.204:5002';
 //const API_URL='http://172.20.10.6:5002';
 //const API_URL='http://192.168.30.131:5002';
